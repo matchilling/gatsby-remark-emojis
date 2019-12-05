72e2bca1cb154941668a8b19ca23dc114c346ff9
Colin Grant <colin.grant@ericsson.com>
2019-12-05 12:53:52 -0600

    Add word break option.

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const source = (size) => require(`./resource/emoji.${size}.json`);
 
 module.exports = {
-  mutateSource: ({ markdownNode }, pluginOptions) => {
-    const active = undefined !== pluginOptions
-            && pluginOptions.hasOwnProperty('active')
-            && false !== pluginOptions.active;
+  mutateSource: ({ markdownNode }, pluginOptions = {}) => {
+    const active = !!pluginOptions.active;
 
     if (!active) return Promise.resolve();
 
-    const classAttribute = pluginOptions && pluginOptions.class
-            ? `class="${pluginOptions.class}"`
-            : null,
-          size = pluginOptions && pluginOptions.size ? pluginOptions.size : 64,
-          styleAttribute = Object.keys(pluginOptions && pluginOptions.styles || {})
+    const classAttribute = pluginOptions.class
+            ? ` class="${pluginOptions.class}" `
+            : "",
+          size = pluginOptions.size || 64,
+          styleAttribute = Object.keys(pluginOptions.styles || {})
             .filter(key => '_PARENT' !== key)
             .map((key) => `${key}: ${pluginOptions.styles[key]}`)
             .join('; '),
@@ -20,9 +18,8 @@ module.exports = {
 
     Object.keys(emojis).forEach((key) => {
       const emoji = emojis[key],
-            pattern = new RegExp(emoji.pattern, 'g'),
-            replacement = (classAttribute ? `<img ${classAttribute} ` : '<img ') + `alt="emoji-${key}" data-icon="emoji-${key}" style="${styleAttribute}" src="data:image/png;base64, ${emojis[key].data}" title="emoji-${key}" />`;
-
+            pattern = pluginOptions.requireWhiteSpace ? new RegExp(`(?<=[\\s\\n])${emoji.pattern}(?:[\\s\\n])`, 'g') : new RegExp(emoji.pattern, 'g'),
+            replacement = `<img${classAttribute} alt="emoji-${key}" data-icon="emoji-${key}" style="${styleAttribute}" src="data:image/png;base64, ${emojis[key].data}" title="emoji-${key}" />`;
 
       markdownNode.internal.content = markdownNode.internal.content.replace(
         pattern,
